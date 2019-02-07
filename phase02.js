261a53f19a92e21c659d2ce5dbaba1d4437afc18
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-02-06 20:02:22 -0800

    Added the for loop for the picture array

--- phase02.js
+++ phase02.js
@@ -1,9 +1,14 @@
 var picURL = ["1.jpg","2.jpg","3.jpg"];
 var picCaption = ["Hot Car","Great Bike","A power brick"];
 var picDesc = ["A car that no one wanted", "A bike that was well loved but ultimately broke", "Neither wanted or needed but existed"];
-var image = document.createElement("img");
-image.setAttribute("src", picURL[0]);
 
-document.getElementById("picture").appendChild(image);
+//iterates over the picture array and adds them to the DOM
+for (var i = 0; i < picURL.length; i++){
+    var image = document.createElement("img");
+    image.setAttribute("src", picURL[i]);
+    document.getElementById("picture").appendChild(image);
 
+    var caption = document.createElement("p");
+    
+}
 
