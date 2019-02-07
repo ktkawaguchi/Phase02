bc585e1bb784780321f346273eb0a37d650749b2
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-02-06 19:32:35 -0800

    Added more HTML, sample pictures, and set up adding in JavaScript

---
+++ phase02.js
@@ -0,0 +1,9 @@
+var picURL = ["1.jpg","2.jpg","3.jpg"];
+var picCaption = ["Hot Car","Great Bike","A power brick"];
+var picDesc = ["A car that no one wanted", "A bike that was well loved but ultimately broke", "Neither wanted or needed but existed"];
+var image = document.createElement("img");
+image.setAttribute("src", picURL[0]);
+
+document.getElementById("picture").appendChild(image);
+
+
