c01c8d927071676bab3e943ed6eb93a2227acc0f
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-02-07 17:59:26 -0800

    Added link to the images, some work with URL parameters

--- phase02.js
+++ phase02.js
@@ -10,10 +10,21 @@ for (var i = 0; i < picURL.length; i++){
     document.getElementById("picture").appendChild(newDiv);
 
     //creates the image and adds it to the div container
+    // var url = "details.html?pic=" + "\"" + picURL[i] + 
+    // "\"+caption=" + "\"" + picCaption[i] + "\"+desc=" + "\"" + picDesc[i] + "\"";
+    var url = "details.html?pic=three four five";
+console.log(url);
+    var a = document.createElement("a");
+    a.setAttribute("href",url);
+    a.setAttribute("id","imageLink" + i);
+    document.getElementById("container"+i).appendChild(a);
+
     var image = document.createElement("img");
+
+
     image.setAttribute("src", picURL[i]);
     image.setAttribute("id","picture" + i);
-    document.getElementById("container"+i).appendChild(image);
+    document.getElementById("imageLink"+i).appendChild(image);
 
     //creates the caption and adds it to the div container
     var caption = document.createElement("p");
